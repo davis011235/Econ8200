d03dc183a65eb4615ffe152c51d6e85d61e8ef68
davis011235 <157519728+davis011235@users.noreply.github.com>
2024-05-12 16:07:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,10 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/davis011235/Econ8200/main/df.csv"
 data = pd.read_csv(url)
 
-st.scatter_chart(data = data, x = "PEDIPGED", y = "PEHRUSLT")
+x = st.sidebar.selectbox(
+    'x variable',
+    ([col for col in data.columns])
+y = st.sidebar.selectbox(
+    'y variable',
+    ([col for col in data.columns])
+st.scatter_chart(data = data, x = x , y = y)
