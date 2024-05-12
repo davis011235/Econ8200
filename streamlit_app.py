d0ef8b7182c5ce8709792db45ac3b95bd1630353
davis011235 <157519728+davis011235@users.noreply.github.com>
2024-05-11 20:10:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 
-url = "url = "https://raw.githubusercontent.com/davis011235/Econ8200/main/df.csv?token=GHSAT0AAAAAACRMRKOZ62D2RPU3CQ3IO4DKZSAC3WA""
+url = "https://raw.githubusercontent.com/davis011235/Econ8200/main/df.csv?token=GHSAT0AAAAAACRMRKOZ62D2RPU3CQ3IO4DKZSAC3WA"
 data = pd.read_csv(url)
 
 st.write(data)
